24ffbbdb45baf906b751793096a0a3e99a3f079d
jonsavagek36 <jonsavagek36@gmail.com>
2017-03-01 10:20:46 -0500

    added dirs

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
